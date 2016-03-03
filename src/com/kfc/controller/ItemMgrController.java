7550dc08b81ce299a949506f3058f38a5cf2823b
JunFengDuan <jfd851590981@gmail.com>
2016-03-03 21:53:19 +0800

    可以接受商品消息队列值

--- src/com/kfc/controller/ItemMgrController.java
+++ src/com/kfc/controller/ItemMgrController.java
@@ -1,10 +1,5 @@
 package com.kfc.controller;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -15,14 +10,14 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.json.JSONObject;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 
-@Controller
 public class ItemMgrController {
 	
-	public void queryItem()throws Exception{
+	public static void queryItem()throws Exception{
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		ConnectionFactory factory = (ConnectionFactory) context.getBean("targetConnectionFactory");
 		Connection conn = factory.createConnection();
@@ -34,36 +29,28 @@ public class ItemMgrController {
 		consumer.setMessageListener(new MessageListener() {
 
 			@Override
-			public void onMessage(Message arg0) {
+			public void onMessage(Message msg) {
 
-				TextMessage msg = (TextMessage) arg0;
-				try {
-					String s = msg.getText();
-					//JSONObject json = new JSONObject(s);
-					//System.out.println(json.get("ip"));
-					msg.acknowledge();
-					// for testing write log
-					FileWriter fw = null;
-					File f = new File("consumer.log");
-
+				TextMessage tm = (TextMessage) msg;
+		
 					try {
-						if (!f.exists()) {
-							f.createNewFile();
-						}
-						fw = new FileWriter(f,true);
-						BufferedWriter out = new BufferedWriter(fw);
-						out.write("\r\n" + s, 0, s.length() - 1);
-						out.close();
-					} catch (IOException e) {
+						String s = tm.getText();
+						JSONObject json = new JSONObject(s);
+						String itemName = (String) json.get("itemName");
+						//Double unitPrice = (Double) json.get("unitPrice");
+						//int amount = (int) json.get("amount");
+						System.out.println(itemName);
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					System.out.println("end");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+					
 			}
 		});
 
+	}
+	
+	public static void main(String[] args) throws Exception {
+		queryItem();
 	}
 	
 }
--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.kfc.controller.ItemMgrController;
 import com.kfc.service.UserService;
 import com.kfc.vo.User;
 
